1affe5218dd13c695093611008783dab34390a0d
mmandyck88 <mealva20@asu.edu>
2025-06-25 04:28:42 +0200

    Create InvalidTreeException.java

---
+++ InvalidTreeException.java
@@ -0,0 +1,14 @@
+/** Purpose of Program: Binary Tree Program checker. Implements a second constructor to create a tree from a string
+ * Revise preorder method. Implements isMaxHeap, isBinarySearchTree, inorderList, and Main method.
+* Author: Melissa Mandyck Instructor: Duane Jarc  *Date: 06/24/2025 
+*Modifications: none      
+ */
+
+// CMSC315PROJ3MandyckM Java File
+
+public class InvalidTreeException extends Exception {
+    public InvalidTreeException(String message) {
+        super(message);
+    }
+}
+//ends CMSC315PROJ3MandyckM Java File
